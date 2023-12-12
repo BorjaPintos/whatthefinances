9de272e31a7b2eba3e7a7839ca10df0e42a9cab0
Borja Pintos Castro <bpintos@gradiant.org>
2023-12-12 10:22:26 +0100

    añadido gráfica de resumen ahorros, ingresos, gastos

--- application/web/static/assets/js/resumen.js
+++ application/web/static/assets/js/resumen.js
@@ -9,45 +9,100 @@ $(document).ready(function() {
 
 
 
-    $.get("finanzas/resumen/total?begin_fecha="+begin_date+"&end_fecha="+end_date, function( data ) {
+    $.get("finanzas/resumen/total?begin_fecha="+begin_date+"&end_fecha="+end_date, function( totales ) {
+        $.get("finanzas/resumen/total-ingresos?begin_fecha="+begin_date+"&end_fecha="+end_date, function( ingresos ) {
+            $.get("finanzas/resumen/total-gastos?begin_fecha="+begin_date+"&end_fecha="+end_date, function( gastos ) {
 
-        var labels = []
-        var datos = {}
-        var fecha_iterada = new Date(año_antes_today)
-        for (var i=1;i<=13;i++){
-             label = (fecha_iterada.getMonth()+1) + "/" + fecha_iterada.getFullYear()
-             labels.push(label)
-             datos[label] = 0
-             fecha_iterada.setMonth(fecha_iterada.getMonth()+1)
-        }
-        for (var i=data.length-1;i>=0;i--){
-            resumen = data[i];
-            label = resumen.mes+"/"+resumen.año
-            datos[label] = resumen.total
-        }
+                var labels = []
+                var datos_totales = {}
+                var datos_ingresos = {}
+                var datos_gastos = {}
+                var fecha_iterada = new Date(año_antes_today)
+                for (var i=1;i<=13;i++){
+                     label = (fecha_iterada.getMonth()+1) + "/" + fecha_iterada.getFullYear()
+                     labels.push(label)
+                     datos_totales[label] = 0
+                     datos_ingresos[label] = 0
+                     datos_gastos[label] = 0
+                     fecha_iterada.setMonth(fecha_iterada.getMonth()+1)
+                }
+                for (var i=totales.length-1;i>=0;i--){
+                    label = totales[i].mes+"/"+totales[i].año
+                    datos_totales[label] = totales[i].total
+                }
+                for (var i=ingresos.length-1;i>=0;i--){
+                    label = ingresos[i].mes+"/"+ingresos[i].año
+                    datos_ingresos[label] = ingresos[i].total
+                }
+                for (var i=gastos.length-1;i>=0;i--){
+                    label = gastos[i].mes+"/"+gastos[i].año
+                    datos_gastos[label] = -gastos[i].total
+                }
 
-        var datos_grafica = []
-        for (var i in labels){
-            datos_grafica.push(datos[labels[i]])
-        }
+                var datos_grafica_totales = []
+                var datos_grafica_ingresos = []
+                var datos_grafica_gastos = []
+                for (var i in labels){
+                    label = labels[i]
+                    datos_grafica_totales.push(datos_totales[label])
+                    datos_grafica_ingresos.push(datos_ingresos[label])
+                    datos_grafica_gastos.push(datos_gastos[label])
+                }
 
-        new Chart(ctx, {
-          type: 'line',
-          data: {
-            labels: labels,
-            datasets: [{
-              label: 'Meses',
-              data: datos_grafica,
-              borderWidth: 1
-            }]
-          },
-          options: {
-            scales: {
-              y: {
-                beginAtZero: true
-              }
-            }
-          }
+                new Chart(ctx, {
+                  type: 'line',
+                  data: {
+                    labels: labels,
+                    datasets: [{
+                      label: 'Ahorro',
+                      data: datos_grafica_totales,
+                      borderColor: '#AED6F1',
+                      backgroundColor: '#AED6F1',
+                      borderWidth: 2,
+                      yAxisID: 'y',
+                    },
+                    {
+                      label: 'Ingresos',
+                      data: datos_grafica_ingresos,
+                      borderColor: '#82E0AA',
+                      backgroundColor: '#82E0AA',
+                      borderWidth: 2,
+                      yAxisID: 'y1',
+                    },
+                    {
+                      label: 'Gastos',
+                      data: datos_grafica_gastos,
+                      borderColor: '#F5B7B1',
+                      backgroundColor: '#F5B7B1',
+                      borderWidth: 2,
+                      yAxisID: 'y2',
+                    }]
+                  },
+                  options: {
+                    responsive: true,
+                    interaction: {
+                      mode: 'index',
+                      intersect: false,
+                    },
+                    stacked: false,
+                    scales: {
+                      y: {
+                        type: 'linear',
+                        display: true,
+                        position: 'left',
+                      },
+                      y1: {
+                        type: 'linear',
+                        display: false,
+                      },
+                      y2: {
+                        type: 'linear',
+                        display: false,
+                      }
+                    }
+                  }
+                });
+            });
         });
     });
 
