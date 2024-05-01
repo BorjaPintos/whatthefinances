bc9c6edf32b2c7ace6e6c86dd448af68cd2257e9
Borja Pintos Castro <bpintos@gradiant.org>
2024-05-01 10:25:53 +0200

    poner sqls por defecto

--- data/sql/finanzas/cuenta.sql
+++ data/sql/finanzas/cuenta.sql
@@ -1,5 +1,6 @@
-insert into finanzas_cuentas ("nombre", "cantidad_inicial", "ponderacion") values ('Ahorro', 2000.00, 10);
-insert into finanzas_cuentas ("nombre", "cantidad_inicial", "ponderacion") values ('Básicos', 0.00, 60);
-insert into finanzas_cuentas ("nombre", "cantidad_inicial", "ponderacion") values ('Formación', 0.00, 10);
-insert into finanzas_cuentas ("nombre", "cantidad_inicial", "ponderacion") values ('Inversión', 0.00, 10);
-insert into finanzas_cuentas ("nombre", "cantidad_inicial", "ponderacion") values ('Ocio', 0.00, 10);
+insert into finanzas_cuentas ("nombre", "cantidad_inicial", "ponderacion") values ('Ahorro',   0.0, 10);
+insert into finanzas_cuentas ("nombre", "cantidad_inicial", "ponderacion") values ('Básicos', 0.0, 50);
+insert into finanzas_cuentas ("nombre", "cantidad_inicial", "ponderacion") values ('Formación', 0.0, 10);
+insert into finanzas_cuentas ("nombre", "cantidad_inicial", "ponderacion") values ('Inversión', 0.0, 10);
+insert into finanzas_cuentas ("nombre", "cantidad_inicial", "ponderacion") values ('Ocio', 0.0, 10);
+insert into finanzas_cuentas ("nombre", "cantidad_inicial", "ponderacion") values ('Fondo de emergencia', 0.0, 10);
--- data/sql/finanzas/monedero.sql
+++ data/sql/finanzas/monedero.sql
@@ -1,5 +1,3 @@
-insert into finanzas_monederos ("nombre", "cantidad_inicial") values ('Cuenta Banco Borja', 0.00);
+insert into finanzas_monederos ("nombre", "cantidad_inicial") values ('Cuenta Banco', 0.00);
 insert into finanzas_monederos ("nombre", "cantidad_inicial") values ('Paypal', 0.00);
-insert into finanzas_monederos ("nombre", "cantidad_inicial") values ('Retriplus', 0.00);
 insert into finanzas_monederos ("nombre", "cantidad_inicial") values ('Efectivo', 0.00);
-insert into finanzas_monederos ("nombre", "cantidad_inicial") values ('Degiro', 0.00);
