f8b1852546a96a3bfa10ee40dce37e99c7065041
Borja Pintos Castro <bpintos@gradiant.org>
2024-06-02 13:02:32 +0200

    actualizado action pyinstaller

--- .github/actions/pyinstaller/pyinstaller.sh
+++ .github/actions/pyinstaller/pyinstaller.sh
@@ -2,4 +2,4 @@
 
 mkdir pyinstaller
 cd pyinstaller
-pyinstaller --noconfirm --onefile --console --name "WTFinances" --add-data "../execonfig.json;." --add-data "../data;data/" --add-data "../application/web/templates;templates/" --add-data "../application/web/static;static/" "../mainexe.py"
+pyinstaller --noconfirm --onefile --console --name "WTFinances" --add-data "../execonfig.json:." --add-data "../data:data/" --add-data "../application/web/templates:templates/" --add-data "../application/web/static:static/" "../mainexe.py"
